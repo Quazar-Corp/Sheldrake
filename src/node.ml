1f43a489ebbdf7da954c0fe88e9d2aa3314046f0
Tiago Onofre <taraujonofre@gmail.com>
2021-10-05 15:21:05 -0300

    Started interfacing with C

--- src/node.ml
+++ src/node.ml
@@ -1,10 +1,16 @@
-type address = string
+type address = {
+  hostname : string;
+  address : string;
+}[@@deriving yojson]
 
 type t = address list
+
+(* External function to retrieve ip address with global scope *)
+external get_global_addr : string = "ocaml_get_global_addr"
 
 (* Add a new node to the network *)
 let add_node address nodes =
   address :: nodes |> fun _ -> address
 
-let retrieve_host_address =
+let retrieve_host_hostname =
   Unix.gethostname ()
