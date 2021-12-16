87fea6f6dcaf9038fa3d359755c9e37271cfde92
Tiago Onofre <taraujonofre@gmail.com>
2021-12-15 21:57:14 -0300

    Removed ecdsa module due to decisions about the signature in transactions and wallets

--- ecdsa/ecdsa.mli
+++
@@ -1,9 +0,0 @@
-(* Elliptic curve parameters -> SECP256K1 *)
-
-(* Auxiliar function to operations *)
-val pow : int -> int -> int
-
-(* Impure function that affects primitive values of the curve *)
-val change_curve : p:int -> n:int64 -> a:int -> b:int -> gx:int64 -> gy:int64 -> unit
-
-
