ebaebbeb7a755067745d7d8f80d1cf1cbcd19bb2
Tiago Onofre Araujo <taraujonofre@gmail.com>
2023-06-08 21:09:32 -0300

    feat: genesis block generation and verification (#25)
    * feat: refacted node and transaction list types
    
    * block module refact and removed deprecated modules
    
    * moved migrations queries to query module
    
    * feat: genesis block generation and verification
    
    * rebased

--- src/database/query.ml
+++ src/database/query.ml
@@ -1,5 +1,50 @@
 open Drake
 
+let ensure_table_chain_exists =
+  [%rapper
+    execute
+      {sql|
+                CREATE TABLE IF NOT EXISTS chain (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    index INT NOT NULL, 
+                    timestamp VARCHAR NOT NULL,
+                    nonce INT NOT NULL,
+                    merkle_root VARCHAR NOT NULL,
+                    transactions JSONB NULL,
+                    prev_hash VARCHAR NOT NULL,
+                    hash VARCHAR NOT NULL
+                );
+            |sql}]
+    ()
+
+let ensure_table_mempool_exists =
+  [%rapper
+    execute
+      {sql|
+                CREATE TABLE IF NOT EXISTS mempool (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    sender VARCHAR NOT NULL,
+                    recipient VARCHAR NOT NULL,
+                    amount FLOAT NOT NULL,
+                    timestamp VARCHAR NOT NULL,
+                    key VARCHAR NOT NULL,
+                    signature VARCHAR NOT NULL
+                );
+            |sql}]
+    ()
+
+let ensure_table_network_exists =
+  [%rapper
+    execute
+      {sql|
+                CREATE TABLE IF NOT EXISTS network (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    hostname VARCHAR NOT NULL,
+                    address VARCHAR NOT NULL
+                );
+            |sql}]
+    ()
+
 let read_network =
   [%rapper
     get_many
